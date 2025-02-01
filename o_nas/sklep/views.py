24fc93a94ac532a16db8f8c257273152db5a5607
michalloo <hordziejewiczmichal7@gmail.com>
2025-02-01 15:34:16 +0100

    zrbione o nas

---
+++ o_nas/sklep/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+def o_nas(request):
+    return render(request, 'o_nas.html')
