89312a1ce575ee520c43bb0dd17e0819df87e20b
michalloo <hordziejewiczmichal7@gmail.com>
2025-02-02 20:05:41 +0100

    szablon jak mniej wiecej bedzie wygladalo w zachodnipomosrkim

---
+++ szablon/sklep/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('o_nas/', views.o_nas, name='o_nas'),
+     path('region/<str:region_id>/', views.region_view, name='region_view'),
+    path('', views.home, name='home'),
+]
